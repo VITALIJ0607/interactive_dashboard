27b154f8628898d67b1982ec38f85c5d49c25e26
Vitalij Zavjalov <vitalijzavjalov@gmail.com>
2024-12-18 16:16:22 +0100

    Add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect, MouseEventHandler } from "react";
+import Grid from "./components/Grid";
 
 function App() {
+  const [rows, setRows] = useState(5);
+  const [columns, setColumns] = useState(5);
+  const [gridItems, setGridItems] = useState<boolean[][]>([]);
+
+  const updateGridItems = (rows: number, columns: number) => {
+    const newGridItems = Array.from({ length: rows }).map(() =>
+      Array.from({ length: columns }).map(() => false)
+    );
+    setGridItems(newGridItems);
+  };
+
+  useEffect(() => {
+    updateGridItems(rows, columns);
+    console.log(gridItems);
+  }, [rows, columns]);
+
+  const AddGridRow = () => {
+    setRows((prevRows) => prevRows + 1);
+  };
+
+  const AddGridColumn = () => {
+    setColumns((prevColumns) => prevColumns + 1);
+  };
+
+  const RemoveGridRow = () => {
+    setRows((prevRows) => Math.max(prevRows - 1, 0));
+  };
+
+  const RemoveGridColumn = () => {
+    setColumns((prevColumns) => Math.max(prevColumns - 1, 0));
+  };
+
+  const toggleCell = (rowIndex: number, columnIndex: number) => {
+    const newGridItems = [ ...gridItems];
+    newGridItems[rowIndex][columnIndex] = !gridItems[rowIndex][columnIndex];
+    setGridItems(newGridItems);
+  };
 
   return (
-    <div></div>
+    <div>
+      <h1>Interaktives Dashboard für Grid-Visualisierung</h1>
+      <div>
+        <button onClick={AddGridRow}>Zeile hinzufügen</button>
+        <button onClick={RemoveGridRow} disabled={rows === 1}>
+          Zeile entfernen
+        </button>
+        <button onClick={AddGridColumn}>Spalte hinzufügen</button>
+        <button onClick={RemoveGridColumn} disabled={columns === 1}>
+          Spalte entfernen
+        </button>
+      </div>
+      <Grid items={gridItems} toggleCell={toggleCell}></Grid>
+    </div>
   );
 }
 
---
+++ src/components/Grid.tsx
@@ -0,0 +1,35 @@
+//import "./Grid.css";
+
+import { MouseEventHandler } from "react";
+
+interface Props {
+  items: boolean[][];
+  toggleCell: (rowIndex: number, columnIndex: number) => void;
+}
+
+const Grid = ({ items, toggleCell}: Props) => {
+  return (
+    <div>
+      {items.map((row, rowIndex) => (
+        <div key={rowIndex} style={{ display: "flex" }}>
+          {row.map((cell, columnIndex) => (
+            <div
+              key={columnIndex}
+              onClick={() => toggleCell(rowIndex, columnIndex)}
+              style={{
+                border: "1px solid black",
+                padding: "10px",
+                margin: "2px",
+                background: cell ? "green" : "grey"
+              }}
+            >
+              {cell}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Grid;
